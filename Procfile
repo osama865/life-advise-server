1a3a03cdfc6b304d2b0fe3bd8e1ae2ab5a5b243a
osama ibrahim <osama@Device-F58698.osama>
2022-05-06 19:49:26 +0200

    try

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node database/index.js
+web: node index.js
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const { MongoClient } = require("mongodb");
 const port = 3001
 
 const fs = require("fs");
@@ -15,6 +16,29 @@ const readFile = () => {
 
 readFile();
 
+
+let advs = []
+app.get('/data', (req, res) => {
+    res.send(advs)
+})
+
+app.get('/', (req, res) => {
+    res.send('hey')
+  })
+const url =
+    "mongodb+srv://advice:XLUoDAWlrhoUjcaH@cluster0.ezstx.mongodb.net/life?retryWrites=true&w=majority";
+
+let collectionName = "advices";
+let dbName = "life";
+
+MongoClient.connect(url, (err, res) => {
+    if (err) throw new Error(err);
+    const db = res.db(dbName);
+    const collection = db.collection(collectionName);
+    db.collection(collection).find().toArray().then((res) => {
+        advs = res
+    })
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
