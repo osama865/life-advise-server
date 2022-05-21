d2537a88bed41d0acd657401e675d3bf64506b9e
osama ibrahim <osama@Device-F58698.osama>
2022-05-21 19:01:11 +0200

    CORS error solved --3

--- notifications.js
+++ notifications.js
@@ -12,6 +12,12 @@ app.use(cors({
 app.use(bodyParser.json())
 const port = process.env.PORT || 3002
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // six hours
 const time = 21600000;
 
